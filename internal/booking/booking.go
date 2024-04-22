23c864f620adc753bb2c9b0eec0bf23d460748d5
Sergey Popov <quolpr@gmail.com>
2024-04-23 00:32:39 +0300

    Add comments about context

--- internal/booking/booking.go
+++ internal/booking/booking.go
@@ -8,6 +8,31 @@ import (
 	"github.com/quolpr/booking/internal/booking/service"
 )
 
+// If I need notification, then notification context will be, and this will require this interface:
+// type OrderCreatedNotifier interface {
+// 	NotifyOrderCreated(ctx context.Context, order model.Order) error
+// }
+
+// OR
+
+// Also I can put event to kafka that order was created,
+// And then other bounded context will process it
+
+// OR
+
+// I could write cross-domain BookingService that will be calling both billing and booking
+
+// ----
+
+// Also, for this:
+
+// - появятся скидки, промокоды, программы лояльности
+
+// 1. I would create billing domain
+// 2. I would create cross-domain service, that will be calling both billing and booking
+// OR
+// 1. Billing will be emitting Paid event, then booking will be listening to this event
+
 // This is Bounded Context
 
 type Domain struct {
